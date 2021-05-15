bb9d1d9ff53658c4aa2f3970d7be4266fbe54da9
shqiperonsadiku <74976264+shqiperonsadiku@users.noreply.github.com>
2021-05-16 00:12:21 +0200

    cracking caesar code

--- Form1.cs
+++ Form1.cs
@@ -47,7 +47,7 @@ namespace siguriaProjektiDesktop
         private void getFrequencyBtn_Click(object sender, EventArgs e)
         {
             String allText = textBox1.Text;
-            allText = allText.ToUpper();
+            allText = allText.ToUpper();       
             allText = allText.Replace('.'.ToString(), string.Empty);
             allText = allText.Replace(','.ToString(), string.Empty);
             allText = allText.Replace('-'.ToString(), string.Empty);
@@ -55,7 +55,7 @@ namespace siguriaProjektiDesktop
             allText = allText.Replace('?'.ToString(), string.Empty);
             allText = allText.Replace('\''.ToString(), string.Empty);
             allText = allText.Replace(' '.ToString(), string.Empty);
-            //allText.Split('.', ' ', ',', '-', '!', '?');            
+      
 
             while (allText.Length>0)
             {
@@ -69,11 +69,14 @@ namespace siguriaProjektiDesktop
                     } 
                 }               
                 add(countOfChar.ToString("D5"), allText[0].ToString());
+                chart1.ChartAreas.FirstOrDefault().AxisX.Interval = 1;                
+                chart1.Series["Letters"].Points.AddXY(allText[0].ToString(), countOfChar);                
                 allText = allText.Replace(allText[0].ToString(), string.Empty);
             }
-            
-            listView1.Columns.Add("Letter Frequency", 110);
-            listView1.Columns.Add("Letter", 100);                   
+
+       
+            listView1.Columns.Add("Letter \nFrequency", 95);
+            listView1.Columns.Add("Letter", 40);                   
             listView1.View = View.Details;
             listView1.GridLines = true;
             listView1.FullRowSelect = true;
@@ -85,6 +88,68 @@ namespace siguriaProjektiDesktop
         {
             textBox1.Clear();
             listView1.Clear();
-        }          
+            chart1.Series[0].Points.Clear();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart2.ChartAreas.FirstOrDefault().AxisX.Interval = 1;
+            chart2.Series["Letters"].Points.AddXY('A'.ToString(), 8.12);
+            chart2.Series["Letters"].Points.AddXY('B'.ToString(), 1.49);
+            chart2.Series["Letters"].Points.AddXY('C'.ToString(), 2.71);
+            chart2.Series["Letters"].Points.AddXY('D'.ToString(), 4.32);
+            chart2.Series["Letters"].Points.AddXY('E'.ToString(), 12.02);
+            chart2.Series["Letters"].Points.AddXY('F'.ToString(), 2.30);
+            chart2.Series["Letters"].Points.AddXY('G'.ToString(), 2.03);
+            chart2.Series["Letters"].Points.AddXY('H'.ToString(), 5.92);
+            chart2.Series["Letters"].Points.AddXY('I'.ToString(), 7.31);
+            chart2.Series["Letters"].Points.AddXY('J'.ToString(), 0.10);
+            chart2.Series["Letters"].Points.AddXY('K'.ToString(), 0.69);
+            chart2.Series["Letters"].Points.AddXY('L'.ToString(), 3.98);
+            chart2.Series["Letters"].Points.AddXY('M'.ToString(), 2.61);
+            chart2.Series["Letters"].Points.AddXY('N'.ToString(), 6.95);
+            chart2.Series["Letters"].Points.AddXY('O'.ToString(), 7.68);
+            chart2.Series["Letters"].Points.AddXY('P'.ToString(), 1.82);
+            chart2.Series["Letters"].Points.AddXY('Q'.ToString(), 0.11);
+            chart2.Series["Letters"].Points.AddXY('R'.ToString(), 6.02);
+            chart2.Series["Letters"].Points.AddXY('S'.ToString(), 6.28);
+            chart2.Series["Letters"].Points.AddXY('T'.ToString(), 9.10);
+            chart2.Series["Letters"].Points.AddXY('U'.ToString(), 2.88);
+            chart2.Series["Letters"].Points.AddXY('V'.ToString(), 1.11);
+            chart2.Series["Letters"].Points.AddXY('W'.ToString(), 2.09);
+            chart2.Series["Letters"].Points.AddXY('X'.ToString(), 0.17);
+            chart2.Series["Letters"].Points.AddXY('Y'.ToString(), 2.11);
+            chart2.Series["Letters"].Points.AddXY('Z'.ToString(), 0.07);
+        }
+
+        private char Encrypt(char ch, int code)
+        {
+            if (!char.IsLetter(ch)) return ch;
+
+            char offset = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((ch + code - offset) % 26 + offset);
+        }
+
+        private string Encrypt(string input, int code)
+        {
+            return new string(input.Select(ch => Encrypt(ch, code)).ToArray());
+        }
+
+        private string Decrypt(string input, int code)
+        {
+            return Encrypt(input, 26 - code);
+        }
+
+        private void enterBtn_Click(object sender, EventArgs e)
+        {
+            decryptedText.Text = Decrypt(textBox1.Text, int.Parse(keyValueTxt.Text));
+        }
+
+        private void clearKeyBtn_Click(object sender, EventArgs e)
+        {
+            keyValueTxt.Clear();
+            decryptedText.Clear();
+        }
     }
 }
